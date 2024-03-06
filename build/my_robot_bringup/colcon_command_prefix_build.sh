e879ad759ac48f45c26b779db45fe506351339e8
moun20 <imenmabrouk72@gmail.com>
2024-03-06 10:07:55 +0100

    first commit

---
+++ build/my_robot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/imen/ros_test/install/my_robot_description/share/my_robot_description/package.sh"
---
+++ build/my_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/imen/ros_test/src/my_robot_description
+# Build directory: /home/imen/ros_test/build/my_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
